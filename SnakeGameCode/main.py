65b7bef3908120042907f77e207237fe4a527a46
Nickel <n_ahn2@u.pacific.edu>
2021-12-16 16:13:27 -0800

    added window caption

--- SnakeGameCode/main.py
+++ SnakeGameCode/main.py
@@ -9,12 +9,13 @@ if __name__ == "__main__":
     pygame.init()
     size = width, height = 480, 480
     screen = pygame.display.set_mode(size=size)
+    pygame.display.set_caption("Snake Game AI")
     color = (255, 255, 255) 
     
-    # while True:
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             break
-    #     #! Do not run or else it'll cause an infinite loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                break
+        #! Do not run or else it'll cause an infinite loop
         
 # TODO: Switch Window Focus to newly created window
