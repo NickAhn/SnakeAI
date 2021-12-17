f36352baa8b7779c5afbfc15d5a6ab21e793f76c
Ramon Arambula <r_arambula@u.pacific.edu>
2021-12-17 15:49:18 -0800

    Added buttons on menu screen

--- SnakeGameCode/main.py
+++ SnakeGameCode/main.py
@@ -1,20 +1,8 @@
-import os
-import pygame
-import menu
+from menu import *
+
 
 
 # Window Creation
-
 if __name__ == "__main__":
-    pygame.init()
-    size = width, height = 480, 480
-    screen = pygame.display.set_mode(size=size)
-    color = (255, 255, 255) 
-    
-    # while True:
-    #     for event in pygame.event.get():
-    #         if event.type == pygame.QUIT:
-    #             break
-    #     #! Do not run or else it'll cause an infinite loop
+    mainGame(fullscreen=False)
         
-# TODO: Switch Window Focus to newly created window
