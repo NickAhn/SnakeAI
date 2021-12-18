619aee56f5ee97ceeb9c0d71297081cba750801f
Ramon Arambula <r_arambula@u.pacific.edu>
2021-12-17 16:01:56 -0800

    Changed main to reflect the menu file class

--- SnakeGameCode/main.py
+++ SnakeGameCode/main.py
@@ -4,19 +4,4 @@ from menu import *
 
 # Window Creation
 if __name__ == "__main__":
-<<<<<<< HEAD
     mainGame(fullscreen=False)
-=======
-    pygame.init()
-    size = width, height = 480, 480
-    screen = pygame.display.set_mode(size=size)
-    pygame.display.set_caption("Snake Game AI")
-    color = (255, 255, 255) 
-    
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                break
-        #! Do not run or else it'll cause an infinite loop
->>>>>>> 65b7bef3908120042907f77e207237fe4a527a46
-        
